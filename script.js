bbc62203692185224bf2312c61df231941115d74
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-25 10:37:34 -0400

    HTML shell

---
+++ script.js
@@ -0,0 +1,16 @@
+// Create a current date in the header with id="currentDay" class="lead"
+
+var currentDate = moment();
+var displayCurrentDate = document.querySelector("#currentDay");
+displayCurrentDate.innerHTML = currentDate.format('dddd MMM Do YYYY');
+
+
+// Create 9 rows with a class="row", containing 3: col-2, col-8 class, button col-2 
+
+
+
+//For the middle column: 1.create a onclick () that adds text 2. create a function that checks the time. 
+
+//create createTask() with taskList = $(<li) append to it addTask=$(<span).text.val
+
+//Save the value inside each row that saves when you press button in third column ensuring that it saves after refresh
