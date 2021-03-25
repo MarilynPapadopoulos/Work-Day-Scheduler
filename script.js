b16f5b88e4f45f2cdc52e0205167352699e89e5a
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-25 12:55:16 -0400

    askBCS

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ displayCurrentDate.innerHTML = currentDate.format('dddd MMM Do YYYY');
 
 //For the middle column: 1.create a onclick () that adds text 2. create a function that checks the time. 
 
+$("#textarea").click(function() {
+    console.log("clicked");
+})
+
+//Save button to save contents of text area to local storage
+
+$("#saveBtn").click(function(){
+    var text = $("#textarea").val();
+    console.log(text);
+    localStorage.setItem("UserInput", text);
+});
+
+// Get UserInput to remain after page refreshes
+localStorage.getItem("UserInput");
+
 //create createTask() with taskList = $(<li) append to it addTask=$(<span).text.val
 
 //Save the value inside each row that saves when you press button in third column ensuring that it saves after refresh
+
+// Add time to time blocks
+
+//if time is past, present or future, apply correct color
