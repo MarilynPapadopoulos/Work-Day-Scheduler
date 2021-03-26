ca5e8b9dd3be2ac592aeb0692f73f25f2acee51a
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-26 15:06:22 -0400

    ASKBcs

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ displayCurrentDate.innerHTML = currentDate.format('dddd MMM Do YYYY');
 //For the middle column: 1.create a onclick () that adds text 2. create a function that checks the time. 
 
 $("#textarea").click(function() {
-    console.log("clicked");
 });
 
-//Save button to save contents of text area to local storage
-$(".saveBtn").on("click", function(){
-    var timeSlotId = $(this).id;
+//When the button is clicked i want to save your sibliling with id textarea to local storage - figure out how to store multiple UserInput
+$('#saveBtn').on('click', function(){
+    var btnClicked = $(this).sibling('#textarea').val();
     console.log($(this));
-    console.log(timeSlotId);
+    console.log(btnClicked);
+
+    localStorage.setItem('UserInput', btnClicked );   
+
 });
 
 
