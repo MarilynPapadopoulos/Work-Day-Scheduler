846c6916844deb81427a240aac9558f54d04d1f3
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-27 14:54:10 -0400

    refactor storage code

--- script.js
+++ script.js
@@ -1,136 +1,24 @@
 // Create a current date in the header with id="currentDay" class="lead"
 
 var currentDate = moment();
-var displayCurrentDate = document.querySelector("#currentDay");
+var displayCurrentDate = document.querySelector('#currentDay');
 displayCurrentDate.innerHTML = currentDate.format('dddd MMM Do YYYY');
 
 
-$('#textarea').click(function() {
-});
 
 //When the button is clicked the textarea is saved to local storage 
-$('#nineBtn').on('click', function(){
+$('.saveBtn').on('click', function(){
     var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
+    var time = $(this).prev().attr('id');
     
-    localStorage.setItem('9:00', btnClicked );     
+    localStorage.setItem(time, btnClicked );     
 });
-//saves when page is refreshed
-var valSaved = localStorage.getItem('9:00');
-    $('.nine').val(valSaved);
 
-//When the button is clicked the textarea is saved to local storage 
-$('#tenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('10:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem("10:00");
-$('.ten').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#elevenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('11:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem("11:00");
-$('.eleven').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#twelveBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('12:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem("12:00");
-$('.twelve').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#thirteenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('1:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem('1:00');
-$('.thirteen').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#fourteenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('2:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem('2:00');
-$('.fourteen').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#fifteenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('3:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem('3:00');
-$('.fifteen').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#sixteenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('4:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem('4:00');
-$('.sixteen').val(valSaved);
-
-//When the button is clicked the textarea is saved to local storage 
-$('#seventeenBtn').on('click', function(){
-    var btnClicked = $(this).prev().val();
-    console.log($(this));
-    console.log(btnClicked);
-
-    var hourlyInput =[];
-
-    localStorage.setItem('5:00', btnClicked );   
-});
-//saves when page is refreshed
-var valSaved = localStorage.getItem('5:00');
-$('.seventeen').val(valSaved);
+//saves when page is refreshed and cycles through all time blocks
+for (var i=9; i <19; i ++) {
+    var valSaved = localStorage.getItem(i);
+    $('#'+i).text(valSaved);
+}
 
 
 // Add time to time blocks
