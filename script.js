d4d3b85281232b8477200b73f657810540a64357
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-28 12:16:45 -0400

    working code

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var currentDate = moment();
 var displayCurrentDate = document.querySelector('#currentDay');
 displayCurrentDate.innerHTML = currentDate.format('dddd MMM Do YYYY');
 
+var currentTime= parseInt(moment().format('H'));
 
 
 //When the button is clicked the textarea is saved to local storage 
@@ -18,114 +19,125 @@ $('.saveBtn').on('click', function(){
 for (var i=9; i <19; i ++) {
     var valSaved = localStorage.getItem(i);
     $('#'+i).text(valSaved);
+
+    if(currentTime === i){
+        $('#'+i).addClass('present');   
+    }
+    if (currentTime < i) {
+        console.log("hello");
+        $('#'+i).addClass('future');
+    }
+    if (currentTime > i) {
+        $('#'+i).addClass('past');
+    }
 }
 
 
 // Add time to time blocks
 
-var currentTime= parseInt(moment().format('H'));
 
 
-var timeBlockNine = document.querySelector('.nine');
-var timeBlockTen = document.querySelector('.ten');
-var timeBlockEleven = document.querySelector('.eleven');
-var timeBlockTwelve = document.querySelector('.twelve');
-var timeBlockThirteen = document.querySelector('.thirteen');
-var timeBlockFourteen = document.querySelector('.fourteen');
-var timeBlockFifteen = document.querySelector('.fifteen');
-var timeBlockSixteen = document.querySelector('.sixteen');
-var timeBlockSeventeen = document.querySelector('.seventeen');
 
-if(currentTime === 9){
-    $(timeBlockNine).addClass('present');   
-}
-if (currentTime < 9) {
-    console.log("hello");
-    $(timeBlockNine).addClass('future');
-}
-if (currentTime > 9) {
-    $(timeBlockNine).addClass('past');
-}
+// var timeBlockNine = document.querySelector('.nine');
+// var timeBlockTen = document.querySelector('.ten');
+// var timeBlockEleven = document.querySelector('.eleven');
+// var timeBlockTwelve = document.querySelector('.twelve');
+// var timeBlockThirteen = document.querySelector('.thirteen');
+// var timeBlockFourteen = document.querySelector('.fourteen');
+// var timeBlockFifteen = document.querySelector('.fifteen');
+// var timeBlockSixteen = document.querySelector('.sixteen');
+// var timeBlockSeventeen = document.querySelector('.seventeen');
 
-
-if(currentTime === 10){
-    $(timeBlockTen).addClass("present");
-    console.log(currentTime);
-}
-if (currentTime < 10) {
-    $(timeBlockTen).addClass("future");
-}
-if (currentTime > 10) {
-    $(timeBlockTen).addClass("past");
-}
+// if(currentTime === 9){
+//     $(timeBlockNine).addClass('present');   
+// }
+// if (currentTime < 9) {
+//     console.log("hello");
+//     $(timeBlockNine).addClass('future');
+// }
+// if (currentTime > 9) {
+//     $(timeBlockNine).addClass('past');
+// }
 
 
-if(currentTime === 11){
-    $(timeBlockEleven).addClass("present");  
-}
-if (currentTime < 11) {
-    $(timeBlockEleven).addClass("future");
-}
-if (currentTime > 11) {
-    $(timeBlockEleven).addClass("past");
-}
+// if(currentTime === 10){
+//     $(timeBlockTen).addClass("present");
+//     console.log(currentTime);
+// }
+// if (currentTime < 10) {
+//     $(timeBlockTen).addClass("future");
+// }
+// if (currentTime > 10) {
+//     $(timeBlockTen).addClass("past");
+// }
 
-if(currentTime === 12){
-    $(timeBlockTwelve).addClass("present");    
-}
-if (currentTime < 12) {
-    $(timeBlockTwelve).addClass("future");
-}
-if (currentTime > 12) {
-    $(timeBlockTwelve).addClass("past");
-}
 
-if(currentTime === 13){
-    $(timeBlockThirteen).addClass("present");   
-}
-if (currentTime < 13) {
-    $(timeBlockThirteen).addClass("future");
-}
-if (currentTime > 13) {
-    $(timeBlockThirteen).addClass("past");
-}
+// if(currentTime === 11){
+//     $(timeBlockEleven).addClass("present");  
+// }
+// if (currentTime < 11) {
+//     $(timeBlockEleven).addClass("future");
+// }
+// if (currentTime > 11) {
+//     $(timeBlockEleven).addClass("past");
+// }
 
-if(currentTime === 14){
-    $(timeBlockFourteen).addClass("present");   
-}
-if (currentTime < 14) {
-    $(timeBlockFourteen).addClass("future");
-}
-if (currentTime > 14) {
-    $(timeBlockFourteen).addClass("past");
-}
+// if(currentTime === 12){
+//     $(timeBlockTwelve).addClass("present");    
+// }
+// if (currentTime < 12) {
+//     $(timeBlockTwelve).addClass("future");
+// }
+// if (currentTime > 12) {
+//     $(timeBlockTwelve).addClass("past");
+// }
 
-if(currentTime === 15){
-    $(timeBlockFifteen).addClass("present"); 
-}
-if (currentTime < 15) {
-    $(timeBlockFifteen).addClass("future");
-}
-if (currentTime > 15) {
-    $(timeBlockFifteen).addClass("past");
-}
+// if(currentTime === 13){
+//     $(timeBlockThirteen).addClass("present");   
+// }
+// if (currentTime < 13) {
+//     $(timeBlockThirteen).addClass("future");
+// }
+// if (currentTime > 13) {
+//     $(timeBlockThirteen).addClass("past");
+// }
 
-if(currentTime === 16){
-    $(timeBlockSixteen).addClass("present");
-}
-if (currentTime < 16) {
-    $(timeBlockSixteen).addClass("future");
-}
-if (currentTime > 16) {
-    $(timeBlockSixteen).addClass("past");
-}
+// if(currentTime === 14){
+//     $(timeBlockFourteen).addClass("present");   
+// }
+// if (currentTime < 14) {
+//     $(timeBlockFourteen).addClass("future");
+// }
+// if (currentTime > 14) {
+//     $(timeBlockFourteen).addClass("past");
+// }
 
-if(currentTime === 17){
-    $(timeBlockSeventeen).addClass("present");
-}
-if (currentTime < 17) {
-    $(timeBlockSeventeen).addClass("future");
-}
-if (currentTime > 17) {
-    $(timeBlockSeventeen).addClass("past");
-}
+// if(currentTime === 15){
+//     $(timeBlockFifteen).addClass("present"); 
+// }
+// if (currentTime < 15) {
+//     $(timeBlockFifteen).addClass("future");
+// }
+// if (currentTime > 15) {
+//     $(timeBlockFifteen).addClass("past");
+// }
+
+// if(currentTime === 16){
+//     $(timeBlockSixteen).addClass("present");
+// }
+// if (currentTime < 16) {
+//     $(timeBlockSixteen).addClass("future");
+// }
+// if (currentTime > 16) {
+//     $(timeBlockSixteen).addClass("past");
+// }
+
+// if(currentTime === 17){
+//     $(timeBlockSeventeen).addClass("present");
+// }
+// if (currentTime < 17) {
+//     $(timeBlockSeventeen).addClass("future");
+// }
+// if (currentTime > 17) {
+//     $(timeBlockSeventeen).addClass("past");
+// }
